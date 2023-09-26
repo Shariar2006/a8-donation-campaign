c1b0698d3e0da8764da90470f02cca6574765698
Shariar2006 <shariarnafis86@gmail.com>
2023-09-26 15:49:04 +0600

    added sweetalerl

--- src/components/SingleCard/Card.jsx
+++ src/components/SingleCard/Card.jsx
@@ -1,27 +1,61 @@
 import React from 'react';
+import swal from 'sweetalert';
 
 const Card = ({ card }) => {
-    const { id, title, description, img, text_color, } = card || {}
+    const { id, title, description, img, text_color, price } = card || {}
+
+    const addToDonation = () => {
+
+        const addDonation = []
+
+        const donateItem = JSON.parse(localStorage.getItem('donate'))
+
+
+        if (!donateItem) {
+            addDonation.push(card)
+            localStorage.setItem('donate', JSON.stringify(addDonation))
+            swal("Good job!", "You have donated successfully!", "success")
+            
+        }
+        else {
+
+            const isExist = donateItem?.find(card => card.id === id)
+
+
+            if (isExist) {
+                swal("Error!", "You have already donated!", "error")
+            }
+            else {
+                addDonation.push(...donateItem, card)
+                localStorage.setItem('donate', JSON.stringify(addDonation))
+                swal("Good job!", "You have donated successfully!", "success")
+                
+            }
+
+
+        }
+
+    }
+
     return (
         <div>
             <div className="relative flex max-w-[1000px] mx-auto flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
                 <div className="overflow-hidden  bg-clip-border text-white ">
-                <img className='w-full mx-auto'
+                    <img className='w-full mx-auto'
                         src={img}
                     />
                 </div>
-                <div className="p-6 pt-3 absolute mt-[229px] md:mt-[442px] lg:mt-[543px] lg:h-20 w-full bg-black/60">
-                        <button style={{ backgroundColor: text_color }}
-                            className="absolute  w-[100px] lg:w-[180px] select-none rounded-lg p-1 lg:p-4  text-center align-middle font-sans text-xs lg:text-sm font-bold uppercase text-white"
-                            type="button"
-                            data-ripple-light="true"
-                        >
+                <div className="p-6 pt-3 absolute mt-[209px] md:mt-[419px] lg:mt-[538px] md:h-10 lg:h-20 w-full bg-black/60">
+                    <button style={{ backgroundColor: text_color }}
+                        className="absolute -mt-3 lg:mt-1  w-[100px] lg:w-[180px] select-none rounded-lg p-1 lg:p-4  text-center align-middle font-sans text-[10px] lg:text-sm font-bold uppercase text-white"
+                        onClick={addToDonation}
+                    >
 
-{/* -mt-10 ml-8 md:-mt-12 md:ml-20 lg:-mt-20 lg:ml-36 */}
 
-                            Donate $290
-                        </button>
-                    </div>
+
+                        Donate ${price}.00
+                    </button>
+                </div>
                 <div className="p-2">
                     <div className="mb-3 flex items-center justify-between">
                         <h5 className="block text-2xl font-bold leading-snug tracking-normal text-black antialiased">
